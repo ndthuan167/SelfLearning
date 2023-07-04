9bcdd18a7b2f99d7acef777f35add53ddf9eb93b
Thuan Nguyen Dinh <126804223+ndthuan167@users.noreply.github.com>
2023-07-04 10:19:54 +0700

    Update PointOfIntersectingWire.c
    Correctly the logical July-4

--- Other/PointOfIntersectingWire.c
+++ Other/PointOfIntersectingWire.c
@@ -14,10 +14,13 @@ void main()
     {
         scanf("%d", &numberofwire[i]);
 
-        for (int j = 0; j < numberofwire[i]; j++)
+        for (int l = 0; l < numberofwire[i]; l++)
         {
-            scanf("%d %d", &higherofnode[j][0], &higherofnode[j][1]);
-            for (int k = 0; k < numberofwire[i]; k++)
+            scanf("%d %d", &higherofnode[l][0], &higherofnode[l][1]);
+        }
+        for (int j = 0; j < numberofwire[i] - 1; j++)
+        {
+            for (int k = j; k < numberofwire[i]; k++)
             {
                 if ((higherofnode[j][0] < higherofnode[k][0]) && (higherofnode[j][1] > higherofnode[k][1]) ||
                     (higherofnode[j][0] > higherofnode[k][0]) && (higherofnode[j][1] < higherofnode[k][1]))
@@ -27,6 +30,7 @@ void main()
             }
         }
     }
+
     for (int i = 0; i < N; i++)
     {
         printf("%d\n", result[i]);
