afb91cf2e9488e2fea8368cc91a9e869986958b3
NDThuan <thuan.ng.167@gmail.com>
2023-08-21 07:52:17 +0700

    Update code for tool dryer gen code 08/20

--- Other/test_toolpython.py
+++ Other/test_toolpython.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QFileDialog, QMessageBox, QFontComboBox, QFontDialog
 from PyQt5 import QtCore
 from PyQt5 import uic
 import sys
@@ -7,7 +7,8 @@ import numpy as np
 import xlrd
 import re
 from enum import Enum
-
+from PyQt5.QtWidgets import * 
+from PyQt5.QtGui import * 
 pulsenumbercourse = []
 listcoursenodata = []
 listcourseadded = []
@@ -17,16 +18,9 @@ course = []
 text = []
 textnew = []
 course_no_data = ['A', '0', '0', '0', '0', '0', '0']
-
+numberOfReadFile = 0
+numberAddlistcourse = 0
 listcourseconvert = []
-
-listcourse = ["COURSE_NORMAL", "COURSE_TIMEDRY", "COURSE_HEAVYDUTY", "COURSE_QUICKDRY", "COURSE_DELICATES",
-              "COURSE_SANITIZE_WITH_STEAM", "COURSE_WRINKLE_AWAY",
-              "COURSE_REFRESH", "COURSE_TOWELS", "COURSE_BEDDING", "COURSE_PET_CARE", "COURSE_JEAN", "COURSE_SHIRTS",
-              "COURSE_WOOL", "COURSE_PERMPRESS",
-              "COURSE_ECONORMAL", "COURSE_AIRFLUFF", "COURSE_RACKDRY", "COURSE_SANITIZE", "COURSE_LOW_TEMP",
-              "COURSE_CLOUD", "COURSE_ACTIVEWEAR", "COURSE_WRINKLE_RELEASE", "COURSE_SIZE"]
-
 
 def ChangeCourseNameCOURSE(coursenamebefore):
     if (coursenamebefore == "Normal"):
@@ -65,7 +59,6 @@ def ChangeCourseNameCOURSE(coursenamebefore):
         coursenamebefore = "COURSE_SANITIZE"
 
     return coursenamebefore
-
 
 def ChangeCourseNamecourse(coursenamebefore):
     if (coursenamebefore == "COURSE_NORMAL"):
@@ -122,77 +115,89 @@ def ChangeCourseNamecourse(coursenamebefore):
         coursenamebefore = "Pet Care"
 
     return coursenamebefore
-
-
-wb = xlrd.open_workbook('C:/Users/dinhthuan.ng/PycharmProjects/pythonProject/test.xlsx')
-sheet = wb.sheet_by_index(0)
-
-for indexx in range(2, sheet.nrows):
-    for indexy in range(0, sheet.ncols):
-        course1.insert(indexy, sheet.cell_value(indexx, indexy))
-    course.append(course1)
-    course1 = []
-
-for each_course in course:
-    for index in range(1, len(each_course)):
-        each_course[index] = "".join(re.findall('\d', each_course[index]))
-
-for index in range(0, len(course)):
-    pulsenumbercourse.insert(index, ChangeCourseNameCOURSE(course[index][0]))
-
-coursenotuse = np.setdiff1d(pulsenumbercourse, listcourse)
-
-for index in range(len(course), len(listcourse) - 1):
-    course.insert(index, course_no_data)
-for index in range(0, len(course)):
-    listcourseadded.insert(index, ChangeCourseNameCOURSE(course[index][0]))
-
-listcoursenodata = set(listcourseadded) ^ set(listcourse)
-listcoursenodata.remove('COURSE_SIZE')
-listcoursenodata.remove('A')
-
-listcourseadded[len(listcourseadded) - len(listcoursenodata): len(listcourseadded)] = listcoursenodata
-
-for index in range(0, len(listcourse)):
-    for indec in range(0, len(listcourseadded)):
-        if (listcourse[index] == listcourseadded[indec]):
-            IndexList.insert(index, indec)
-
-
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
-        uic.loadUi("UI_Tool.ui", self)
+        uic.loadUi("D:/LearningCode/C_programming/Other/UI_Tool.ui", self)
 
         self.Text = "This box will show mcau16NoMeetAddTimeByPulse array after gen"
         self.textEdit.setPlainText(self.Text)
-        self.pushButton.clicked.connect(self.clickme)
-        self.pushButton_2.clicked.connect(self.showtext)
+        self.textEdit.setFont(QFont('Consolas', 10))
+        self.pushButton.clicked.connect(self.ShowArrayAfterGenFunction)
+        self.pushButton_2.clicked.connect(self.GetListCourseAndArrageFunction)
+        self.pushButton_3.clicked.connect(self.ReloadExcelfile)
         self.show()
 
-    def showtext(self):
+    def ReloadExcelfile(self):
+        global numberOfReadFile
+        numberOfReadFile += 1
+        os.startfile('D:/LearningCode/Python/test.xlsx')
+
+    def GetListCourseAndArrageFunction(self):
+        #Show meesage after set List course
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText("List course is set")
+        msgBox.setWindowTitle("Message")
+        msgBox.exec()
+        # After push set list course -> numberAddlistcourse++
+        global course1, numberAddlistcourse
+        numberAddlistcourse += 1
+        # Process and convert the raw list course to list listcourse
         text = self.textEdit_2.toPlainText()
         textnew = str(text).replace("\n", "")
         textnew = textnew.replace("\t", "")
+        textnew = textnew.replace(" ", "")
         listcourseconvert1 = textnew.split(",")
-        return listcourseconvert1
-        # print(abc[0])
+        listcourseconvert1.remove('')
+        # read excel file
+        wb = xlrd.open_workbook('D:/LearningCode/Python/test.xlsx')
+        sheet = wb.sheet_by_index(0)    # process sheet 0
+        # list "course" is a big list, include many "course1" list from data of excel file
+        for indexx in range(2, sheet.nrows):
+            for indexy in range(0, sheet.ncols):
+                course1.insert(indexy, sheet.cell_value(indexx, indexy))
+            course.append(course1)
+            course1 = []
+        # delete charector excep number
+        for each_course in course:
+            for index in range(1, len(each_course)):
+                each_course[index] = "".join(re.findall('\d', each_course[index]))
+        # get the name of list course excel file         
+        for index in range(0, len(course)):
+            pulsenumbercourse.insert(index, ChangeCourseNameCOURSE(course[index][0]))
+        # add empty list to course for other course in excel file
+        for index in range(len(course), len(listcourseconvert1) - 1):
+            course.insert(index, course_no_data)
+        for index in range(0, len(course)):
+            listcourseadded.insert(index, ChangeCourseNameCOURSE(course[index][0]))
+        # find in list course the course has not data in excel file
+        listcoursenodata = set(listcourseadded) ^ set(listcourseconvert1)
+        listcoursenodata.remove('COURSE_SIZE')
+        listcoursenodata.remove('A')
+        # replace the 'A' list = "course not has data" list
+        listcourseadded[len(listcourseadded) - len(listcoursenodata): len(listcourseadded)] = listcoursenodata
+        # find index of course in excel file and other course with the right list course.
+        for index in range(0, len(listcourseconvert1)):
+            for indec in range(0, len(listcourseadded)):
+                if (listcourseconvert1[index] == listcourseadded[indec]):
+                    IndexList.insert(index, indec)
 
-    def clickme(self):
-        self.Text = "PRIVATE const U16 mcau16NoMeetAddTimeByPulse[COURSE_SIZE][INITIAL_PULSE_LEVEL_SIZE] =		//종료 3분 전까지 건조가 충분히 이뤄지지 않으면 아래 조건에 맞도록"
-        self.textEdit.setPlainText(self.Text)
-        self.textEdit.append(
-            "{//		PULSE_LEVEL_0,	PULSE_LEVEL_1,	PULSE_LEVEL_2,	PULSE_LEVEL_3,	PULSE_LEVEL_4,	PULSE_LEVEL_5")
-        for index in range(0, len(listcourseadded)):
-            list = "    {   MIN(" + str(course[IndexList[index]][1]) + "),    MIN(" + str(
-                course[IndexList[index]][2]) + "),     MIN(" + str(course[IndexList[index]][3]) + "),    MIN(" + str(
-                course[IndexList[index]][4]) + "),    MIN(" + str(course[IndexList[index]][5]) + "),   MIN(" \
-                   + str(course[IndexList[index]][6]) + ") },	//" + str(listcourse[index]) + ",       // " + str(
-                index) + "  " + str(ChangeCourseNamecourse(listcourseadded[IndexList[index]]))
-            self.textEdit.append(list)
-        self.textEdit.append("};")
-
-
+    def ShowArrayAfterGenFunction(self):
+        if numberOfReadFile == 1 and numberAddlistcourse == 1:
+            self.Text = "PRIVATE const U16 mcau16NoMeetAddTimeByPulse[COURSE_SIZE][INITIAL_PULSE_LEVEL_SIZE] =		//종료 3분 전까지 건조가 충분히 이뤄지지 않으면 아래 조건에 맞도록"
+            self.textEdit.setPlainText(self.Text)
+            self.textEdit.append(
+                "{//		PULSE_LEVEL_0,	PULSE_LEVEL_1,	PULSE_LEVEL_2,	PULSE_LEVEL_3,	PULSE_LEVEL_4,	PULSE_LEVEL_5")
+            for index in range(0, len(listcourseadded)):
+                list = "    {   MIN(" + str(course[IndexList[index]][1]) + "),    MIN(" + str(
+                    course[IndexList[index]][2]) + "),     MIN(" + str(course[IndexList[index]][3]) + "),    MIN(" + str(
+                    course[IndexList[index]][4]) + "),    MIN(" + str(course[IndexList[index]][5]) + "),   MIN(" \
+                    + str(course[IndexList[index]][6]) + ") },	//" + str(listcourseadded[IndexList[index]]) + ",       // " + str(
+                    index) + "  " + str(ChangeCourseNamecourse(listcourseadded[IndexList[index]]))
+                self.textEdit.append(list)
+            self.textEdit.append("};")
+            self.textEdit.setFont(QFont('Consolas', 8))
 app = QApplication(sys.argv)
 window = UI()
 app.exec_()
